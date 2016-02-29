f2400a73a6fa796f6b06efec0715c2404356103b
Henryforce <hj_serrano@hotmail.com>
2016-02-29 13:11:22 -0600

    Added Framework

---
+++ OBESDK_iOS.framework/Versions/A/Headers/OBE.h
@@ -0,0 +1,39 @@
+//
+//  OBESDK_iOS.h
+//  OBESDK_iOS
+//
+//  Created by Henry Serrano on 2/25/16.
+//  Copyright © 2016 Machina Wearable Technology SAPI de CV. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreBluetooth/CoreBluetooth.h>
+
+@protocol OBEDelegate <NSObject>
+
+- (void) onOBEFound:(NSString *)name Index:(int)index;
+- (void) onOBEConnected:(NSString *)name;
+- (void) onOBEDisconnected:(NSString *)name;
+- (void) onQuaternionUpdated:(int) identifier W:(float)w X:(float)x Y:(float)y Z:(float)z;
+
+@end
+
+@interface OBE : NSObject<CBCentralManagerDelegate, CBPeripheralDelegate>{
+    CBCentralManager *manager;
+    CBPeripheral *obePeripheral;
+    
+    NSMutableArray *peripherals;
+    
+    BOOL isConnected;
+    float W,X,Y,Z;
+}
+
+@property id<OBEDelegate> delegate;
+
+- (id)initWithDelegate:(id<OBEDelegate>)delegate;
+
+- (void) startScanning;
+- (void) connectToOBE:(int)index;
+- (void) disconnectFromOBE;
+
+@end
